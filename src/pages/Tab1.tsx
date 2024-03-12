fef60170be56a574d451d25b252cc032da2515fd
JairoTinga <adamkirigaya@gmail.com>
2024-03-12 08:04:51 +0800

    Added button and alert

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonAlert, IonButton } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -24,8 +24,17 @@ const Tab1: React.FC = () => {
         <IonCardTitle>Jairo</IonCardTitle>
         <IonCardSubtitle>pee pee poo poo</IonCardSubtitle>
       </IonCardHeader>
-
       <IonCardContent>wowsers, BATCHEST</IonCardContent>
+
+      <IonButton id="present-alert" expand="full" >Click Me</IonButton>
+      <IonAlert
+        trigger="present-alert"
+        header="A Short Title Is Best"
+        subHeader="A Sub Header Is Optional"
+        message="A message should be a short, complete sentence."
+        buttons={['Action']}
+      ></IonAlert>
+
     </IonCard>
 
       </IonContent>
