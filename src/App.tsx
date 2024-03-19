af648f29e161108746e33acc4b35749fddc0d271
JairoTinga <adamkirigaya@gmail.com>
2024-03-19 08:20:57 +0800

    Added import home

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,10 @@ import {
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { calculatorOutline, ellipse, personCircleOutline, pushOutline, square, triangle } from 'ionicons/icons';
+
+// Home resources
+import Home from './pages/home';
+
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
