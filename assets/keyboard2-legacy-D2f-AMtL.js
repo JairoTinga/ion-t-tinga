85f26094f36a4e3c290e5eb6da0eac62056265d6
jkmanait <jkmanait@gmail.com>
2024-05-03 08:50:09 +0800

    Updates

--- assets/keyboard2-legacy-D2f-AMtL.js
+++ assets/keyboard2-legacy-D2f-AMtL.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-COWaqSAY.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-C3x8qMma.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
