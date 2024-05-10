0c2fb6f04488d826ed75f6cde0a48871db775ff0
Jairo Tinga <adamkirigaya@gmail.com>
2024-05-10 08:39:56 +0800

    Updates

--- assets/keyboard2-legacy-D8CyE8oj.js
+++ assets/keyboard2-legacy-D8CyE8oj.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B_NI4Jsy.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-RaBdgRgf.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
